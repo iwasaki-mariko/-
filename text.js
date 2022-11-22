3d68d1a3189fc0cb70880ac925173ed26f34988a
minema12 <79952773+iwasaki-mariko@users.noreply.github.com>
2022-11-22 10:02:40 +0900

    Create text.js

---
+++ text.js
@@ -0,0 +1,18 @@
+const omikuji_items = [
+  "大吉! ",
+  "中吉。",
+  "吉",
+  "小吉",
+  "末吉"
+];
+
+
+const btn = document.getElementById("gacha-button");
+const disp = document.getElementById("gacha-display");
+
+btn.addEventListener("click", function() {
+  const num = Math.floor(Math.random() * omikuji_items.length );
+  disp.innerHTML = omikuji_items[num];
+});
+document.getElementById('gacha-button').onclick = function() {
+    this.textContent = 'もう一度';
